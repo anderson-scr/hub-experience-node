172591016b190618e002d13a9c59a6339e268134
Anderson <anderson.scr@outlook.com>
2022-06-10 10:58:40 -0400

    Adicionando CORS

--- routes/Database/server.js
+++ routes/Database/server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
 const dbService = require("./dbService")
 const dotenv = require("dotenv")
 dotenv.config()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
